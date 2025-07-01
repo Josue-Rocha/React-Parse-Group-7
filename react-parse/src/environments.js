9546a32a04b9219354240a09b6bb8b353a982a43
Tariq Fuller <69810093+tariqfuller1@users.noreply.github.com>
2025-07-01 18:37:48 -0400

    Basic Parse inside App.js

---
+++ react-parse/src/environments.js
@@ -0,0 +1,5 @@
+module.exports={
+    APPLICATION_ID: "7vhFYxH0AUCHStoTK4s9QOrFSrJdC8UxnuUuHblh",
+    JAVASCRIPT_KEY: "PP7EHTEAYYM26AgK2bBIUzPGZlebZtJspVwZzOtr",
+    SERVER_URL: "https://parseapi.back4app.com",
+}
