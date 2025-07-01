fef0b74ffd07bc1b26e6f1ad2e99f7f18f562fa8
Tariq Fuller <69810093+tariqfuller1@users.noreply.github.com>
2025-07-01 18:13:54 -0400

    React app

--- src/Components/Components.js
+++
@@ -1,9 +0,0 @@
-import Main from "./Main/Main.js";
-
-const Components = () => {
-    return (
-        <div>
-            <Main />
-        </div>
-    )
-}
--- src/Services/LearnService.js
+++
@@ -1,9 +0,0 @@
-import Parse from "parse";
-
-export const getAllLessons = () => {
-    const Lesson = Parse.Object.extend("Lesson");
-    const query = new Parse.Query(Lesson)
-    return query.find().then((results) => {
-        return results;
-    })
-}
